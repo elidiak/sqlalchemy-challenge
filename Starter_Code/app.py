fbcf3630925a5614dfefb2be95fa75ee42c3c479
Eric Lidiak <el8472@gmail.com>
2024-08-28 19:27:21 -0600

    Started work on app.py

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,30 +1,57 @@
 # Import the dependencies.
-
-
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+import pandas as pd
+import datetime as dt
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func,  inspect, text
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(autoload_with=engine)
 
 # reflect the tables
-
+Measurment = Base.classes.measurement
+Station = Base.classes.station
 
 # Save references to each table
-
+Measurment = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
